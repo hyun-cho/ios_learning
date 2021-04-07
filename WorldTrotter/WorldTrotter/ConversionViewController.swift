0bd391bd0b4487895903655740a56914eec76d20
hyun-cho <hyun__cho@naver.com>
2021-04-07 16:52:40 +0900

    Chapter4 delegation 추가 완료
    delegation을 위해 UITextFieldDelegation 사용
    - 소숫점 자리를 위한 point는 1개만 작성가능
    - 알파벳 입력 불가능

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -8,8 +8,70 @@
 import Foundation
 import UIKit
 
-class ConversionViewController: UIViewController {
+// 델리게이트 프로토콜을 사용해 사용한다.(Delegate가 붙여진 프로토콜 붙인다.)
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        
+        // if let text = textField.text where !text.isEmpty {
+        // Error??? : Expected ',' joining parts of a multi-clause condition
+        /*
+        if let text = textField.text ,!text.isEmpty {
+            celsiusLabel.text = text
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+         */
+        
+        // Double 타입 캐스팅이 되는지 확인, nil 검사
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    // 사용자가 텍스트 필드를 누르는 시점을 알고자한다면 -> textFieldDidBeginEditing(_:)
+    // 입력 요청을 보고자 한다면 -> textField(_:shouldChangeCharactersIn:replacementString:) -> Bool
+    // UITextFieldDelegate에 들어가보면 모든 메서드가 optional로 설정되어 있다.
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        // UITextField.text -> String?
+        // print("current text: \(textField.text!)")
+        // print("replacement text : \(string)")
+        
+        // 두번 째 소수점을 거부하는 delegation
+        // rangeOfString -> contains
+        let existingTextHasDecimalSeparator = textField.text?.contains(".")
+        let replacementTextHasDecimalSeparator = string.contains(".")
+        
+        if existingTextHasDecimalSeparator ?? false == true &&
+            replacementTextHasDecimalSeparator {
+            return false
+        }
+        
+        // lowercase, uppercase 캐릭터셋 확인 -> 더 좋은 방법이 있지 않을까요?
+        var alphaCharacterSet = CharacterSet(charactersIn: "a"..."z")
+        alphaCharacterSet.insert(charactersIn: "A"..."Z")
+        let replacementTextHasCharacterValue = string.rangeOfCharacter(from: alphaCharacterSet)
+        
+        if replacementTextHasCharacterValue != nil{
+            return false
+        }
+        
+        return true
+        
+    }
+
     var fahrenheitValue: Double? {
         didSet {
             updateCelsiusLabel()
@@ -47,33 +109,4 @@ class ConversionViewController: UIViewController {
         }
     }
     
-    
-    @IBOutlet var celsiusLabel: UILabel!
-    @IBOutlet var textField: UITextField!
-    
-    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        
-        // if let text = textField.text where !text.isEmpty {
-        // Error??? : Expected ',' joining parts of a multi-clause condition
-        /*
-        if let text = textField.text ,!text.isEmpty {
-            celsiusLabel.text = text
-        }
-        else {
-            celsiusLabel.text = "???"
-        }
-         */
-        
-        // Double 타입 캐스팅이 되는지 확인, nil 검사
-        if let text = textField.text, let value = Double(text) {
-            fahrenheitValue = value
-        }
-        else {
-            fahrenheitValue = nil
-        }
-    }
-    
-    @IBAction func dismissKeyboard(sender: AnyObject) {
-        textField.resignFirstResponder()
-    }
 }
