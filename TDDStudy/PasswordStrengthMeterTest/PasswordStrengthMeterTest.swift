2c3f37a539eabbf0b879fb365c8f4eb6ec92595e
hyun-cho <hyun__cho@naver.com>
2021-04-16 01:35:12 +0900

    TDD Chapter2 모든 조건을 테스트로 작성 완료

--- TDDStudy/PasswordStrengthMeterTest/PasswordStrengthMeterTest.swift
+++ TDDStudy/PasswordStrengthMeterTest/PasswordStrengthMeterTest.swift
@@ -9,7 +9,8 @@ import XCTest
 @testable import TDDStudy
 
 class PasswordStrengthMeterTest: XCTestCase {
-
+    private var meter: PasswordStrengthMeter = PasswordStrengthMeter()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,26 +18,61 @@ class PasswordStrengthMeterTest: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        
+    
+    // 첫번째 : 모든 조건을 충족하는 경우
+    func testMeetsAllCriteriaThenStrong() throws {
+        assertStrength(password: "ab12!@AB", expStr: .STRONG)
+        assertStrength(password: "abc1!Add", expStr: .STRONG)
     }
     
-    // 모든 조건을 충족하는 경우
-    func meetsAllCriteriaThenStrong() {
-        let meter: PasswordStrengthMeter = PasswordStrengthMeter()
-        let result = meter.meter(password: "ab12!@AB")
-        XCTAssertEqual(PasswordStrength.STRONG, result)
+    // 두번째 : 길이만 8글자 미만이고 나머지 조건은 충족하는 경우
+    func testMeetsOtherCriteriExceptForLengthThenNormal() throws {
+        assertStrength(password: "ab12!@A", expStr: .NORMAL)
     }
+    
+    // 세번째 : 숫자를 포함하지 않고 나머지 조건은 충족하는 경우
+    func testMeetsOtherCriteriaExceptForNumberThenNormal() throws {
+        assertStrength(password: "ab!@ABqwer", expStr: .NORMAL)
+    }
+    
+    // 네번째 : 빈 패스워드가 오는 경우
+    func testNullInputThenInvalid() throws {
+        assertStrength(password: nil, expStr: .INVALID)
+        assertStrength(password: "", expStr: .INVALID)
+    }
+    
+    // 다섯번째 : 대문자를 포함하지 않고 나머지 조건을 충족하는 경우
+    func testMeetsOtherCriteriaExceptForUppercaseThenNormal() {
+        assertStrength(password: "ab12!@df", expStr: .NORMAL)
+    }
+    
+    // 여섯번째 : 길이가 8글자 이상인 조건만 충족하는 경우
+    func testMeetsOnlyLengthCriteriaThenWeak() {
+        assertStrength(password: "abdefghi", expStr: .WEAK)
+    }
+    
+    // 일곱번째 : 숫자 포함 조건만 충족하는 경우
+    func testMeetsOnlyNumCriteriaThenWeak() {
+        assertStrength(password: "12345", expStr: .WEAK)
+    }
+    
+    // 여덟번째 : 대문자 포함 조건만 충족하는 경우
+    func testMeetsOnlyUpperCriteriaThenWeak() {
+        assertStrength(password: "ABZEF", expStr: .WEAK)
+    }
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func assertStrength(password: String?, expStr :PasswordStrength) {
+        let result = meter.meter(password: password)
+        // NORMAL을 추가했고, 오류가 발생했으므로 본 코드를 수정한다.
+        XCTAssertEqual(result, expStr)
     }
 
 }
--- TDDStudy/TDDStudy/PasswordStrengthMeter.swift
+++ TDDStudy/TDDStudy/PasswordStrengthMeter.swift
@@ -9,12 +9,56 @@ import Foundation
 
 
 enum PasswordStrength {
-    case STRONG
+    case STRONG, NORMAL, INVALID, WEAK
 }
 
 class PasswordStrengthMeter {
     
-    func meter(password: String) -> PasswordStrength {
-        return PasswordStrength.STRONG
+    func meter(password: String?) -> PasswordStrength {
+        guard let password = password,
+              !password.isEmpty else {
+            return .INVALID
+        }
+        let lengthEnough = password.count >= 8
+        let containsNum: Bool = meetsContainingNumberCriteria(string: password)
+        let containsUpp: Bool = meetsContainingUppercaseCriteria(string: password)
+        
+        if lengthEnough && !containsUpp && !containsNum {
+            return .WEAK
+        }
+        if !lengthEnough && containsUpp && !containsNum {
+            return .WEAK
+        }
+        if !lengthEnough && !containsUpp && containsNum {
+            return .WEAK
+        }
+        
+        if !lengthEnough {
+            return .NORMAL
+        }
+        if !containsNum {
+            return .NORMAL
+        }
+        if !containsUpp {
+            return .NORMAL
+        }
+        return .STRONG
+    }
+    
+    func meetsContainingNumberCriteria(string: String) -> Bool {
+        for char in string {
+            if char >= "0" && char <= "9" {
+                return true
+            }
+        }
+        return false
+    }
+    func meetsContainingUppercaseCriteria(string: String) -> Bool {
+        for char in string {
+            if char.isUppercase {
+               return true
+            }
+        }
+        return false
     }
 }
