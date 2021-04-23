53183f8d9eb2ea0c8afeeb4f1ff1c7375091c12e
hyun-cho <hyun__cho@naver.com>
2021-04-23 15:37:09 +0900

    TDDStudy 3-4

--- TDDStudy/DateCalculator/DateCalculator.swift
+++ TDDStudy/DateCalculator/DateCalculator.swift
@@ -19,7 +19,7 @@ class DateCalculator: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    // 3.2
+    // 3.4
     func test_만원_납부하면_한달_뒤가_만료일이_됨() throws {
         let billingDate: Date = Date.of(2019, 3, 1)
         let payAmount: Int = 10_000
@@ -28,6 +28,14 @@ class DateCalculator: XCTestCase {
         let expiryDate: Date = cal.calculateExpiryDate(billingDate, payAmount)
         
         XCTAssertEqual(Date.of(2019,4,1), expiryDate)
+        
+        let billingDate2: Date = Date.of(2019,5,5)
+        let payAmount2 = 10_000
+        
+        let cal2: ExpiryDateCalculator = ExpiryDateCalculator()
+        let expiryDate2: Date = cal2.calculateExpiryDate(billingDate2, payAmount2)
+        
+        XCTAssertEqual(Date.of(2019,6,05), expiryDate2)
     }
 
     func testPerformanceExample() throws {
